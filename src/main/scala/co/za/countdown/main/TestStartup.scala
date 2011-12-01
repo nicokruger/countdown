d57c176527e2782fcfb7ef14c9b437e8ec32c8b0
Dawid <dawid.malan@gmail.com>
2011-12-01 19:39:07 +0200

    add static resource from filesystem

--- src/main/scala/co/za/countdown/main/TestStartup.scala
+++ src/main/scala/co/za/countdown/main/TestStartup.scala
@@ -3,6 +3,8 @@ package co.za.countdown.main
 import co.za.countdown.serve.ServeCountdowns
 import java.net.URL
 import unfiltered.jetty.ContextBuilder
+import scalax.io.Resource
+import java.io.File
 
 /**
  * User: dawid
@@ -14,5 +16,7 @@ object TestStartup extends App {
   unfiltered.jetty.Http(55555)
     .context("/static") {
       (builder: ContextBuilder) => builder.resources(new URL(getClass.getResource("/static/"), "."))}
+    .context("/filesystem") {
+      (builder: ContextBuilder) => builder.resources(new URL(new File("/tmp/filesystem/").toURI.toURL, "."))}
     .filter(ServeCountdowns.countdowns).run()
 }
